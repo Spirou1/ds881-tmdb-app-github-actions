172e355309abc7c582d3c43fdb6bc5f377d98f6a
Victor <40972803+victorbetoni@users.noreply.github.com>
2026-04-29 09:24:36 -0300

    Passando bearer na request

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -5,6 +5,10 @@ const TMDB_API_KEY = process.env.EXPO_PUBLIC_TMDB_API_KEY;
 
 export const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+    Accept: 'application/json',
+  },
   params: {
     api_key: TMDB_API_KEY,
     language: 'en',
